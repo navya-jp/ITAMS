90599201d9b60f5460d9fcc6239270e9ee5f4325
Kriyadm <kdmorabia@gmail.com>
2026-04-27 13:11:41 +0530

    chore: remove redundant/superseded migration files

--- Migrations/shift_to_site.sql
+++
@@ -1,8 +0,0 @@
--- Shift assets after ASTH00096 with no placing to site (Lane Area = 1)
--- Assets up to ASTH00096 (Ahmedabad) remain in head office
-UPDATE Assets 
-SET AssetPlacingId = 1 
-WHERE AssetPlacingId IS NULL 
-  AND AssetId > 'ASTH00096';
-
-SELECT @@ROWCOUNT AS UpdatedCount;
